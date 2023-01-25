99a6d0e4d302d342ed2b16225dc21c4954f5f734
fumou20xx <fumou.20xx.tue@gmail.com>
2023-01-25 12:41:06 +0900

    no message

--- Assets/Script/MutualConnect.cs
+++ Assets/Script/MutualConnect.cs
@@ -59,6 +59,7 @@ public class MutualConnect : MonoBehaviour
 
         request.msg = judgeMsg;
         Debug.Log("request.msg =" + request.msg);
+        Publish();
 
         // ROSサーバーへのリクエスト送信
         this.rc.SendServiceMessage<MyServiceResponse>(ServiceName, request, SAResponse);
@@ -80,6 +81,7 @@ public class MutualConnect : MonoBehaviour
     // デバッグ用起床判定
     private void InputKeyNumber()
     {
+        
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             judgeMsg = "get up";
